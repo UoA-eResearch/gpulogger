4b1becd3af0e6ff47bf885d69d6f6d55a4e4fd36
Nick Young <nick.young@auckland.ac.nz>
2018-07-10 16:50:07 +1200

    add an echo to indicate script success

--- add_to_cron.sh
+++ add_to_cron.sh
@@ -3,3 +3,4 @@
 croncmd="curl https://raw.githubusercontent.com/UoA-eResearch/gpulogger/master/query_gpu_and_post.sh | bash"
 cronjob="* * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+echo "GPU logging script added to crontab :)"
