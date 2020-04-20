985f3381c04b39c411b20c78db227a752e0cbe4c
Sean Matheny <seanmatheny@me.com>
2020-04-20 14:48:20 +1200

    Update query_gpu_and_post.sh

--- query_gpu_and_post.sh
+++ query_gpu_and_post.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo nvidia-smi --persistence-mode=ENABLED
-curl -X POST -H "Content-Type: text/csv" -d "`nvidia-smi --query-gpu=timestamp,name,pci.bus_id,index,fan.speed,power.draw,driver_version,pstate,pcie.link.gen.max,pcie.link.gen.current,pcie.link.width.current,pcie.link.width.max,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv,nounits`" gpuusage.nectar.auckland.ac.nz
+curl -X POST -H "Content-Type: text/csv" -d "`nvidia-smi --query-gpu=timestamp,name,pci.bus_id,index,fan.speed,power.draw,driver_version,pstate,pcie.link.gen.max,pcie.link.gen.current,pcie.link.width.current,pcie.link.width.max,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv,nounits`" gpuusage-data.nectar.auckland.ac.nz
