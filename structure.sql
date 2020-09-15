f908a7d6bbdba0d17bd5ef730febfe423525fdda
Martin Feller <m.feller@auckland.ac.nz>
2020-09-16 11:23:59 +1200

    reflect new partitioned gpu_log table in structure

--- structure.sql
+++ structure.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
 --
 -- Host: localhost    Database: gpu
 -- ------------------------------------------------------
--- Server version	5.7.22-0ubuntu18.04.1
+-- Server version	5.7.30-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,13 +34,49 @@ SET character_set_client = utf8;
 SET character_set_client = @saved_cs_client;
 
 --
--- Table structure for table `gpu`
+-- Table structure for table `gpu_booking`
 --
 
-DROP TABLE IF EXISTS `gpu`;
+DROP TABLE IF EXISTS `gpu_booking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gpu` (
+CREATE TABLE `gpu_booking` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(255) DEFAULT NULL,
+  `project_uuid` varchar(255) DEFAULT NULL,
+  `booking_start_date` date DEFAULT NULL,
+  `booking_end_date` date DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `gpu_type` char(16) NOT NULL,
+  `count` int(11) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `project_uuid` (`project_uuid`,`gpu_type`,`booking_start_date`,`booking_end_date`),
+  KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=27844 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gpu_booking_ip2project`
+--
+
+DROP TABLE IF EXISTS `gpu_booking_ip2project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gpu_booking_ip2project` (
+  `gpu_booking_id` int(11) DEFAULT NULL,
+  `ip2project_id` int(11) DEFAULT NULL,
+  UNIQUE KEY `gpu_booking_id` (`gpu_booking_id`,`ip2project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gpu_log`
+--
+
+DROP TABLE IF EXISTS `gpu_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gpu_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(64) DEFAULT NULL,
   `timestamp` varchar(64) DEFAULT NULL,
@@ -62,10 +98,134 @@ CREATE TABLE `gpu` (
   `memory.free [MiB]` int(11) DEFAULT NULL,
   `memory.used [MiB]` int(11) DEFAULT NULL,
   `insertedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`,`insertedAt`),
+  UNIQUE KEY `insertedAt` (`insertedAt`,`ip`,`pci.bus_id`),
   KEY `ip` (`ip`),
-  KEY `insertedAt` (`insertedAt`)
-) ENGINE=InnoDB AUTO_INCREMENT=852878 DEFAULT CHARSET=latin1;
+  KEY `pci.bus_id` (`pci.bus_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6887476 DEFAULT CHARSET=latin1
+/*!50100 PARTITION BY RANGE (UNIX_TIMESTAMP(`insertedAt`))
+(PARTITION p2018_01 VALUES LESS THAN (1517443200) ENGINE = InnoDB,
+ PARTITION p2018_02 VALUES LESS THAN (1519862400) ENGINE = InnoDB,
+ PARTITION p2018_03 VALUES LESS THAN (1522540800) ENGINE = InnoDB,
+ PARTITION p2018_04 VALUES LESS THAN (1525132800) ENGINE = InnoDB,
+ PARTITION p2018_05 VALUES LESS THAN (1527811200) ENGINE = InnoDB,
+ PARTITION p2018_06 VALUES LESS THAN (1530403200) ENGINE = InnoDB,
+ PARTITION p2018_07 VALUES LESS THAN (1533081600) ENGINE = InnoDB,
+ PARTITION p2018_08 VALUES LESS THAN (1535760000) ENGINE = InnoDB,
+ PARTITION p2018_09 VALUES LESS THAN (1538352000) ENGINE = InnoDB,
+ PARTITION p2018_10 VALUES LESS THAN (1541030400) ENGINE = InnoDB,
+ PARTITION p2018_11 VALUES LESS THAN (1543622400) ENGINE = InnoDB,
+ PARTITION p2018_12 VALUES LESS THAN (1546300800) ENGINE = InnoDB,
+ PARTITION p2019_01 VALUES LESS THAN (1548979200) ENGINE = InnoDB,
+ PARTITION p2019_02 VALUES LESS THAN (1551398400) ENGINE = InnoDB,
+ PARTITION p2019_03 VALUES LESS THAN (1554076800) ENGINE = InnoDB,
+ PARTITION p2019_04 VALUES LESS THAN (1556668800) ENGINE = InnoDB,
+ PARTITION p2019_05 VALUES LESS THAN (1559347200) ENGINE = InnoDB,
+ PARTITION p2019_06 VALUES LESS THAN (1561939200) ENGINE = InnoDB,
+ PARTITION p2019_07 VALUES LESS THAN (1564617600) ENGINE = InnoDB,
+ PARTITION p2019_08 VALUES LESS THAN (1567296000) ENGINE = InnoDB,
+ PARTITION p2019_09 VALUES LESS THAN (1569888000) ENGINE = InnoDB,
+ PARTITION p2019_10 VALUES LESS THAN (1572566400) ENGINE = InnoDB,
+ PARTITION p2019_11 VALUES LESS THAN (1575158400) ENGINE = InnoDB,
+ PARTITION p2019_12 VALUES LESS THAN (1577836800) ENGINE = InnoDB,
+ PARTITION p2020_01 VALUES LESS THAN (1580515200) ENGINE = InnoDB,
+ PARTITION p2020_02 VALUES LESS THAN (1583020800) ENGINE = InnoDB,
+ PARTITION p2020_03 VALUES LESS THAN (1585699200) ENGINE = InnoDB,
+ PARTITION p2020_04 VALUES LESS THAN (1588291200) ENGINE = InnoDB,
+ PARTITION p2020_05 VALUES LESS THAN (1590969600) ENGINE = InnoDB,
+ PARTITION p2020_06 VALUES LESS THAN (1593561600) ENGINE = InnoDB,
+ PARTITION p2020_07 VALUES LESS THAN (1596240000) ENGINE = InnoDB,
+ PARTITION p2020_08 VALUES LESS THAN (1598918400) ENGINE = InnoDB,
+ PARTITION p2020_09 VALUES LESS THAN (1601510400) ENGINE = InnoDB,
+ PARTITION p2020_10 VALUES LESS THAN (1604188800) ENGINE = InnoDB,
+ PARTITION p2020_11 VALUES LESS THAN (1606780800) ENGINE = InnoDB,
+ PARTITION p2020_12 VALUES LESS THAN (1609459200) ENGINE = InnoDB,
+ PARTITION p2021_01 VALUES LESS THAN (1612137600) ENGINE = InnoDB,
+ PARTITION p2021_02 VALUES LESS THAN (1614556800) ENGINE = InnoDB,
+ PARTITION p2021_03 VALUES LESS THAN (1617235200) ENGINE = InnoDB,
+ PARTITION p2021_04 VALUES LESS THAN (1619827200) ENGINE = InnoDB,
+ PARTITION p2021_05 VALUES LESS THAN (1622505600) ENGINE = InnoDB,
+ PARTITION p2021_06 VALUES LESS THAN (1625097600) ENGINE = InnoDB,
+ PARTITION p2021_07 VALUES LESS THAN (1627776000) ENGINE = InnoDB,
+ PARTITION p2021_08 VALUES LESS THAN (1630454400) ENGINE = InnoDB,
+ PARTITION p2021_09 VALUES LESS THAN (1633046400) ENGINE = InnoDB,
+ PARTITION p2021_10 VALUES LESS THAN (1635724800) ENGINE = InnoDB,
+ PARTITION p2021_11 VALUES LESS THAN (1638316800) ENGINE = InnoDB,
+ PARTITION p2021_12 VALUES LESS THAN (1640995200) ENGINE = InnoDB,
+ PARTITION p2022_01 VALUES LESS THAN (1643673600) ENGINE = InnoDB,
+ PARTITION p2022_02 VALUES LESS THAN (1646092800) ENGINE = InnoDB,
+ PARTITION p2022_03 VALUES LESS THAN (1648771200) ENGINE = InnoDB,
+ PARTITION p2022_04 VALUES LESS THAN (1651363200) ENGINE = InnoDB,
+ PARTITION p2022_05 VALUES LESS THAN (1654041600) ENGINE = InnoDB,
+ PARTITION p2022_06 VALUES LESS THAN (1656633600) ENGINE = InnoDB,
+ PARTITION p2022_07 VALUES LESS THAN (1659312000) ENGINE = InnoDB,
+ PARTITION p2022_08 VALUES LESS THAN (1661990400) ENGINE = InnoDB,
+ PARTITION p2022_09 VALUES LESS THAN (1664582400) ENGINE = InnoDB,
+ PARTITION p2022_10 VALUES LESS THAN (1667260800) ENGINE = InnoDB,
+ PARTITION p2022_11 VALUES LESS THAN (1669852800) ENGINE = InnoDB,
+ PARTITION p9999 VALUES LESS THAN MAXVALUE ENGINE = InnoDB) */;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gpu_nodes`
+--
+
+DROP TABLE IF EXISTS `gpu_nodes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gpu_nodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hypervisor` varchar(64) NOT NULL,
+  `gpu_type` char(16) NOT NULL,
+  `pci_id` varchar(32) NOT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `tmp_active` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `hypervisor_pci_id` (`hypervisor`,`pci_id`),
+  KEY `hypervisor` (`hypervisor`),
+  KEY `gpu_type` (`gpu_type`)
+) ENGINE=InnoDB AUTO_INCREMENT=90358 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ip2project`
+--
+
+DROP TABLE IF EXISTS `ip2project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ip2project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(64) DEFAULT NULL,
+  `project_name` varchar(255) DEFAULT NULL,
+  `project_uuid` varchar(255) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `instance_uuid` varchar(255) DEFAULT NULL,
+  `instance_name` varchar(255) DEFAULT NULL,
+  `instance_launched_at` datetime DEFAULT NULL,
+  `instance_terminated_at` datetime DEFAULT NULL,
+  `final` int(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip_uuid_triple` (`ip`,`project_uuid`,`instance_uuid`),
+  KEY `ip` (`ip`),
+  KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=88277 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ip2project_gpu_nodes`
+--
+
+DROP TABLE IF EXISTS `ip2project_gpu_nodes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ip2project_gpu_nodes` (
+  `ip2project_id` int(11) NOT NULL,
+  `gpu_node_id` int(11) NOT NULL,
+  PRIMARY KEY (`ip2project_id`,`gpu_node_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +258,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`gpu`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `active_hours` AS select `gpu`.`ip` AS `ip`,cast(`gpu`.`insertedAt` as date) AS `day`,(count(0) / 60) AS `active_hours`,((count(0) / 60) / 24) AS `fraction_of_day`,min(`gpu`.`utilization.gpu [%]`) AS `MIN(``utilization.gpu [%]``)`,max(`gpu`.`utilization.gpu [%]`) AS `MAX(``utilization.gpu [%]``)`,avg(`gpu`.`utilization.gpu [%]`) AS `AVG(``utilization.gpu [%]``)` from `gpu` where (`gpu`.`utilization.gpu [%]` > 0) group by `gpu`.`ip`,`day` order by `day` */;
+/*!50001 VIEW `active_hours` AS select `gpu_log`.`ip` AS `ip`,cast(`gpu_log`.`insertedAt` as date) AS `day`,(count(0) / 60) AS `active_hours`,((count(0) / 60) / 24) AS `fraction_of_day`,min(`gpu_log`.`utilization.gpu [%]`) AS `MIN(``utilization.gpu [%]``)`,max(`gpu_log`.`utilization.gpu [%]`) AS `MAX(``utilization.gpu [%]``)`,avg(`gpu_log`.`utilization.gpu [%]`) AS `AVG(``utilization.gpu [%]``)` from `gpu_log` where (`gpu_log`.`utilization.gpu [%]` > 0) group by `gpu_log`.`ip`,`day` order by `day` desc */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -116,7 +276,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`gpu`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `test_active_hours` AS select `gpu`.`ip` AS `ip`,cast(`gpu`.`insertedAt` as date) AS `day`,cast(`gpu`.`insertedAt` as time) AS `time`,dayname(cast(`gpu`.`insertedAt` as date)) AS `dow`,`gpu`.`utilization.gpu [%]` AS `utilization.gpu [%]`,`gpu`.`utilization.memory [%]` AS `utilization.memory [%]` from `gpu` where (`gpu`.`utilization.gpu [%]` > 0) order by cast(`gpu`.`insertedAt` as date),cast(`gpu`.`insertedAt` as time) desc */;
+/*!50001 VIEW `test_active_hours` AS select `gpu_log`.`ip` AS `ip`,cast(`gpu_log`.`insertedAt` as date) AS `day`,cast(`gpu_log`.`insertedAt` as time) AS `time`,dayname(cast(`gpu_log`.`insertedAt` as date)) AS `dow`,`gpu_log`.`utilization.gpu [%]` AS `utilization.gpu [%]`,`gpu_log`.`utilization.memory [%]` AS `utilization.memory [%]` from `gpu_log` where (`gpu_log`.`utilization.gpu [%]` > 0) order by cast(`gpu_log`.`insertedAt` as date),cast(`gpu_log`.`insertedAt` as time) desc */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -130,4 +290,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-14 11:41:45
+-- Dump completed on 2020-09-16 11:22:59
